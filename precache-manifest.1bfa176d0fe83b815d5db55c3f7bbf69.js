904d816265bf01e51d15f1e0294b984531464ecd
tomnyson <tabletkindfire@gmail.com>
2020-08-04 00:11:03 +0700

    Updates

---
+++ precache-manifest.1bfa176d0fe83b815d5db55c3f7bbf69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d3a4feba6a5e3b8d5f424f7a92b8c0d",
+    "url": "/freecodecamp-dump/index.html"
+  },
+  {
+    "revision": "2b57ce58f139b77e3918",
+    "url": "/freecodecamp-dump/static/css/main.37f22037.chunk.css"
+  },
+  {
+    "revision": "14953a9bbe748cc2c0ce",
+    "url": "/freecodecamp-dump/static/js/2.fa071812.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/freecodecamp-dump/static/js/2.fa071812.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b57ce58f139b77e3918",
+    "url": "/freecodecamp-dump/static/js/main.76ad24ee.chunk.js"
+  },
+  {
+    "revision": "64aef5a52c8a181dc6f7",
+    "url": "/freecodecamp-dump/static/js/runtime-main.d49e6f26.js"
+  }
+]);
